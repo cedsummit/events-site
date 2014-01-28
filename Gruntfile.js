326fa1dbdb8b6f233f7a4108a0db68066be9bf60
Shabladoo <shaun.dowling@gmail.com>
2014-01-28 12:03:40 +0000

    set up tasks for compiling sass and linting

---
+++ Gruntfile.js
@@ -0,0 +1,27 @@
+module.exports = function (grunt) {
+  grunt.initConfig({
+    pkg: grunt.file.readJSON('package.json'),
+
+    jshint: {
+      src: ["src/js/*.js", "Gruntfile.js"]
+    },
+
+    sass: {
+        dist: {
+          files: {
+            'src/css/main.css': 'src/css/sass/main.scss'
+          }
+        }
+    },
+
+    watch:{},
+
+  });
+
+
+  grunt.loadNpmTasks('grunt-contrib-jshint');
+  grunt.loadNpmTasks('grunt-contrib-sass');
+  grunt.loadNpmTasks('grunt-contrib-watch');
+
+  grunt.registerTask('default', ['sass', 'jshint']);
+};
