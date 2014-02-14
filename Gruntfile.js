4f06452f47272a178d975801da30e4e34a9a07f6
Shabladoo <shaun.dowling@gmail.com>
2014-02-14 14:23:07 +0000

    Changed to speaker and topic

--- Gruntfile.js
+++ Gruntfile.js
@@ -27,7 +27,10 @@ module.exports = function (grunt) {
   grunt.loadNpmTasks('grunt-contrib-jshint');
   grunt.loadNpmTasks('grunt-contrib-sass');
   grunt.loadNpmTasks('grunt-contrib-watch');
+  grunt.loadNpmTasks('grunt-shell');
 
   grunt.registerTask('default', ['sass', 'jshint']);
   grunt.registerTask('dev', ['watch']);
+
+
 };
