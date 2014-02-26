0813875a6a3952a795649e14de2b972ca3321506
Shabladoo <shaun.dowling@gmail.com>
2014-02-26 16:43:27 +0000

    team section javascript kitchsink

--- src/js/app.js
+++ src/js/app.js
@@ -21,4 +21,52 @@ $(document).ready(function () {
       }, 500);
       return false;
   });
+
+  // Cache selectors
+  var topMenu = $(".menu"),
+      topMenuHeight = topMenu.height()+200,
+
+      // All list items
+      menuItems = topMenu.find("a"),
+
+      // Anchors corresponding to menu items
+      scrollItems = menuItems.map(function(){
+        var item = $($(this).attr("href"));
+        if (item.length) { return item; }
+      });
+
+      console.log(menuItems);
+      console.log(scrollItems);
+
+  // Bind to scroll
+  $(window).scroll(function(){
+     // Get container scroll position
+     var fromTop = $(this).scrollTop()+topMenuHeight;
+     console.log(fromTop);
+     // Get id of current scroll item
+     var cur = scrollItems.map(function(){
+       if ($(this).offset().top < fromTop)
+         return this;
+     });
+     // Get the id of the current element
+     cur = cur[cur.length-1];
+     var id = cur && cur.length ? cur[0].id : "";
+
+     console.log(id)
+     menuItems.removeClass('active');
+     $('a[href="#'+ id + '"]').addClass('active');
+     // Set/remove active class
+  });
+
+  //Make sure things resize!
+  $( window ).resize(function() {
+    var wHeight = $(window).height(),
+        hHeight = $('header').height(),
+        aboutHeight = wHeight - hHeight;
+
+        $('#about').css('height', wHeight + 'px');
+  });
+
+
+
 });
